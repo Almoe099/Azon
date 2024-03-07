b22d5f73e2cbc67c1d00975b10fe8fd994bf2564
Almoe099 <almoe099@gmail.com>
2024-03-07 16:36:00 -0500

    added review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,14 @@
 class Review < ApplicationRecord
+
+  validates :user_id, :product_id, :review, :rating, presence: true
+  validates :rating, inclusion: {in: 1..5, message: "must be selected"}
+
+  belongs_to :user,
+  foreign_key: :user_id,
+  class_name: :User
+
+  belongs_to :product,
+  foreign_key: :product_id,
+  class_name: :Product
+
 end
