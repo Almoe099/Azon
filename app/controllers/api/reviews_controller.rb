6279d6df3b42556f840387b3032a195d7af86c1c
Almoe099 <almoe099@gmail.com>
2024-03-13 16:08:36 -0400

    added the rest of methods for review controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -5,20 +5,45 @@ class Api::ReviewsController < ApplicationController
     render :index
   end
 
-  def show
-    @review = Review.find_by(id: params[:id])
+  def create
+    @review = Review.new(review_params)
+    @user = User.find_by(id: @review.user_id)
 
-    if @review
+    if @review.save
       render :show
     else
-      render json: {errors: ['Review doesnt exist.']},
-      status: :unauthorized
+      render json: {errors: @review.errors.full_messages}, status: :unprocessable_entity
+    end
+
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @user = User.find_by(id: @review.user_id)
+
+    if @review
+      if @review && @review.user_id == @user.id && @review.update(review_params)
+        render :show
+      else
+        render json: {errors: @review.errors.full_messages}, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def destroy
+    @review = Review.find_by(id: params[:id])
+    if @review.destroy
+      render json: "removed"
+    else
+      render json: {errors: @review.errors.full_messages}
     end
   end
 
 
+  private
 
-
-
+  def review_params
+    params.require(:review).permit(:rating, :review, :user_id, :product_id)
+  end
 
 end
