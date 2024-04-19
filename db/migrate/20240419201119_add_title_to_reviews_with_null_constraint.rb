09981d6651bae67328da733c56da7364e73683f5
Almoe099 <almoe099@gmail.com>
2024-04-19 16:12:31 -0400

    fixed and added title for review migration

---
+++ db/migrate/20240419201119_add_title_to_reviews_with_null_constraint.rb
@@ -0,0 +1,5 @@
+class AddTitleToReviewsWithNullConstraint < ActiveRecord::Migration[7.0]
+  def change
+    add_column :reviews, :title, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_06_182249) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_19_201119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_06_182249) do
     t.integer "rating", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title", null: false
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
