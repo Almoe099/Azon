fe2756518b060f1a1b35dd5a3f7d429f154731af
Almoe099 <almoe099@gmail.com>
2024-05-10 15:46:40 -0400

    removed console log in fetch reviews

--- frontend/src/store/review.js
+++ frontend/src/store/review.js
@@ -50,7 +50,6 @@ export const fetchReviews = () => async dispatch => {
   const res = await csrfFetch('/api/reviews');
   if (res.ok) {
     const data = await res.json();
-    console.log(data.reviews);
     dispatch(receiveReviews(data.reviews));
   }
 };
